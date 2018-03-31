bf24c54815bfa556be785ce83de719bc625249cb
keanemind <keanemind@gmail.com>
2018-03-31 17:47:55 -0500

    Prepare to receive form data

--- site.py
+++ site.py
@@ -1,9 +1,15 @@
-from flask import Flask
+from flask import Flask, request
 app = Flask(__name__)
 
 @app.route("/")
 def index():
     return app.send_static_file('wholesome-webpage.html')
+
+@app.route('/text', methods=['POST'])
+def text():
+    projectpath = request.form['projectFilepath']
+    # your code
+    # return a response
 
 if __name__ == "__main__":
     app.run("localhost", port=80, debug=True)
